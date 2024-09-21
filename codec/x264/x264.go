291658ac42163ea58f3a59cade0213846bf9234e
BradNicolle <bradnicolle@gmail.com>
2024-09-21 11:40:52 +0930

    Add x264 codec.

---
+++ codec/x264/x264.go
@@ -0,0 +1,24 @@
+package x264
+
+import (
+	"github.com/pion/mediadevices"
+	"github.com/pion/mediadevices/pkg/codec/x264"
+
+	"github.com/thingify-app/thing-rtc-go/codec"
+)
+
+func NewCodec(bitrate int) (*codec.Codec, error) {
+	params, err := x264.NewParams()
+	if err != nil {
+		return nil, err
+	}
+	params.BitRate = bitrate
+
+	codecSelector := mediadevices.NewCodecSelector(
+		mediadevices.WithVideoEncoders(&params),
+	)
+
+	return &codec.Codec{
+		CodecSelector: codecSelector,
+	}, nil
+}
